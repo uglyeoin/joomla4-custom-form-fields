57c2f2300527f547e92b05a0184438c7c08ed3e0
Eoin Oliver <eoin@squareballoon.co.uk>
2020-10-16 20:27:52 +0100

    added files

---
+++ media/js/mod_joomla4_module_custom_form_fields_backend.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+    alert('The DOM has loaded')
+})
---
+++ models/forms/fakeusers.php
@@ -0,0 +1,30 @@
+<?php
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\HTML\HTMLHelper;
+
+class JFormFieldMyjavascript extends Joomla\CMS\Form\FormField
+{
+
+    protected $type = 'Myjavascript';
+
+    protected function getLabel()
+    {
+        return;
+    }
+
+    protected function getInput()
+    {
+        // JS
+        HTMLHelper::_('script', 'mod_joomla4_module_custom_form_fields/mod_joomla4_module_custom_form_fields_backend.js', ['version' => 'auto', 'relative' => true]);
+
+        // CSS
+        HTMLHelper::_('stylesheet', 'mod_joomla4_module_custom_form_fields/mod_joomla4_module_custom_form_fields_backend.css', ['version' => 'auto', 'relative' => true]);
+
+        // Custom HTML
+        $html = '<p>Hello, is it me you\'re waiting for</p>';
+
+        return $html;
+    }
+}
