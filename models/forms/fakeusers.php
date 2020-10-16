9f0aa5b347e689a0e600a6e24645a132caf4e964
Eoin Oliver <eoin@squareballoon.co.uk>
2020-10-16 22:47:42 +0100

    cleaned up backend

--- models/forms/fakeusers.php
+++ models/forms/fakeusers.php
@@ -6,6 +6,9 @@ use Joomla\CMS\Uri\Uri;
 
 JFormHelper::loadFieldClass('list');
 
+
+
+
 // The class name must always be the same as the filename (in camel case)
 class JFormFieldFakeusers extends JFormFieldList
 {
@@ -15,38 +18,15 @@ class JFormFieldFakeusers extends JFormFieldList
 
     public function getOptions()
     {
-
-        $results    =   $params->get('results');
-
-        if($results)
-        {
-            $results = "results=" . $results;
-        }
-
-        $gender = "";
-        if ($gender != 0)
-        {
-            $gender = "gender=" . $params->get('gender');
-        }
-
-        $seed = "";
-        if(!empty($gender))
-        {
-            $seed = $params->get('seed');;
-        }
-
-
-        $json = "https://randomuser.me/api/" . $results . $gender . $seed;
+        $json = "https://randomuser.me/api/?results=8&seed=joomla";
         $json = file_get_contents($json);
 
         $people = json_decode($json, true);
 
-        $i = 0;
-        foreach($people['results'[$i]] as $person)
+        foreach($people['results'] as $person)
         {
             $personName = $person['name']['title'] . " " . $person['name']['first'] . " " . $person['name']['last'];
             $fakeUsersOptions[] = array("value" => $personName, "text" => $personName);
-            $i++;
         }
         // Merge any additional options in the XML definition.
         $options = array_merge(parent::getOptions(), $fakeUsersOptions);
